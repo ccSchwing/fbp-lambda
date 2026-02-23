57bf27dba457bbdaf9a315ddb1f977659926fd52
Chuck Schwing <chuckschwing@proton.me>
2026-02-22 21:59:43 -0500

    Converted FBPPickSheet to use POST. Also commented out the rollback in samconfig.toml This was causing stack contamination. Added jackson-databind to Utils pom.xml. Added annotations to FBPPickSheet.

--- Utils/src/main/java/com/fbp/FBPPickSheet.java
+++ Utils/src/main/java/com/fbp/FBPPickSheet.java
@@ -1,5 +1,6 @@
 package com.fbp;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
@@ -17,27 +18,33 @@ public class FBPPickSheet {
 
     @DynamoDbPartitionKey
     @DynamoDbAttribute("Week")
+    @JsonProperty("Week")
     public String getWeek() { return week; }
     public void setWeek(String week) { this.week = week; }
 
     @DynamoDbSortKey
     @DynamoDbAttribute("GameId")
+    @JsonProperty("GameId")
     public String getGameId() { return gameId; }
     public void setGameId(String gameId) { this.gameId = gameId; }
 
     @DynamoDbAttribute("Away")
+    @JsonProperty("Away")
     public String getAwayTeam() { return awayTeam; }
     public void setAwayTeam(String awayTeam) { this.awayTeam = awayTeam; }
     
     @DynamoDbAttribute("Home")
+    @JsonProperty("Home")
     public String getHomeTeam() { return homeTeam; }
     public void setHomeTeam(String homeTeam) { this.homeTeam = homeTeam; }
 
     @DynamoDbAttribute("Date")
+    @JsonProperty("Date")
     public String getDate() { return date; }
     public void setDate(String date) { this.date = date; }
 
     @DynamoDbAttribute("Spread")
+    @JsonProperty("Spread")
     public String getSpread() { return spread; }
     public void setSpread(String spread) { this.spread = spread; }
 }
